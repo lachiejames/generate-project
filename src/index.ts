f3d1410d62e96278448a68f637fab8feb09241c0
lachiejames <lachie_james@hotmail.com.au>
2023-02-21 04:02:56 +1100

    reinstate index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-console.log("Hello World!");
+import { runPostScaffoldSteps } from "./runPostScaffoldSteps";
+import { runScaffold } from "./runScaffold";
+
+runScaffold(process.cwd()).then(() => runPostScaffoldSteps());
