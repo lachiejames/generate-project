e5fb9bc8ac99d945fd9596b232f901ebbaf46601
lachiejames <lachie_james@hotmail.com.au>
2023-02-21 16:09:44 +1100

    update E2E

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,5 +1,22 @@
 import { execSync } from "child_process";
+import { setPromptMock, cleanTestDirectory, TEST_DIRECTORY } from "../testUtils/helpers";
 
-it("generating a ts-library", () => {
-  execSync("ls -la", { stdio: "inherit" });
+export default function runCommand(command: string) {
+  execSync(command, { stdio: "inherit" });
+}
+
+describe("e2e", () => {
+  beforeEach(() => {
+    cleanTestDirectory();
+    setPromptMock();
+  });
+
+  afterEach(() => {
+    cleanTestDirectory();
+  });
+
+  it("generating a ts-library", () => {
+    runCommand(`mkdir ${TEST_DIRECTORY}`);
+    runCommand(`npm pack --pack-destination ${TEST_DIRECTORY}`);
+  });
 });
