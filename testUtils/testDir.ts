d21b3f2f64bb42b07a15a8ecd442e74ec2989e73
lachiejames <lachie_james@hotmail.com.au>
2023-02-22 06:26:22 +1100

    fix failing tests

--- testUtils/testDir.ts
+++ testUtils/testDir.ts
@@ -1,8 +1,6 @@
 import path from "path";
 
-import rootDir from "../src/constants/rootDir";
-
 // WARNING: this directory will be deleted on every test run, so don't put anything important in here
-const testDir = path.join(rootDir, "tempTestDir");
+const testDir = path.normalize(path.join(__dirname, "..", "tempTestDir"));
 
 export default testDir;
