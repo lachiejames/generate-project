80036ca6c01b849546307e17d23fbe8cc7b75b7a
lachiejames <lachie_james@hotmail.com.au>
2023-02-21 17:02:06 +1100

    make test functional

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,23 +1,29 @@
 import { execSync } from "child_process";
+import { sync } from "glob";
 import { setPromptMock, cleanTestDirectory, TEST_DIRECTORY } from "../testUtils/helpers";
 
 function runCommand(command: string) {
   execSync(command, { stdio: "inherit" });
 }
 
+function buildProject() {
+  runCommand("yarn install");
+  runCommand("yarn build");
+}
+
 function uninstallPackageGlobally() {
+  runCommand(`rm -rf *.tgz`);
   runCommand(`npm uninstall -g @lachiejames/generate-project`);
 }
 
 function installPackageGlobally() {
-  runCommand(`mkdir ${TEST_DIRECTORY}`);
-  runCommand(`npm pack --pack-destination ${TEST_DIRECTORY}`);
-  runCommand(`npm install -g ${TEST_DIRECTORY}/*.tgz`);
-  runCommand(`rm -rf ${TEST_DIRECTORY}/*.tgz`);
+  runCommand(`npm pack`);
+  runCommand(`npm install -g *.tgz`);
 }
 
 describe("e2e", () => {
   beforeAll(() => {
+    buildProject();
     uninstallPackageGlobally(); // In case it wasn't already uninstalled
     installPackageGlobally();
   });
@@ -37,7 +43,16 @@ describe("e2e", () => {
 
   it("install package globally then generate a ts-library", () => {
     runCommand(`mkdir ${TEST_DIRECTORY}`);
-    runCommand(`npm pack --pack-destination ${TEST_DIRECTORY}`);
-    runCommand(`npm pack --pack-destination ${TEST_DIRECTORY}`);
+    runCommand(`cd ${TEST_DIRECTORY}`);
+    runCommand(`generate-project`);
+
+    const templateFilePaths: string[] = sync(TEST_DIRECTORY, {
+      dot: true,
+      nodir: true,
+    });
+
+    console.log(templateFilePaths);
+
+    expect(templateFilePaths).toContain(`${TEST_DIRECTORY}/.package.json`);
   });
 });
