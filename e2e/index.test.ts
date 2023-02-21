b8d6bf34824ec9eac7f006a64c596053a55fd9e2
lachiejames <lachie_james@hotmail.com.au>
2023-02-21 17:15:19 +1100

    add another test

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,6 +1,8 @@
 import { execSync } from "child_process";
+import { readFileSync } from "fs-extra";
 import { sync } from "glob";
 import { setPromptMock, cleanTestDirectory, TEST_DIRECTORY } from "../testUtils/helpers";
+import { MOCK_ANSWERS } from "../testUtils/mockAnswers";
 
 function runCommand(command: string) {
   execSync(command, { stdio: "inherit" });
@@ -24,24 +26,26 @@ function installPackageGlobally() {
 describe("e2e", () => {
   beforeAll(() => {
     buildProject();
+
+    cleanTestDirectory(); // In case it wasn't already empty
     uninstallPackageGlobally(); // In case it wasn't already uninstalled
+
     installPackageGlobally();
+  });
+
+  beforeEach(() => {
+    setPromptMock();
   });
 
   afterAll(() => {
     uninstallPackageGlobally();
   });
 
-  beforeEach(() => {
-    cleanTestDirectory(); // In case it wasn't already empty
-    setPromptMock();
-  });
-
   afterEach(() => {
     cleanTestDirectory();
   });
 
-  it("install package globally then generate a ts-library", () => {
+  it("produces the expected files", () => {
     runCommand(`mkdir ${TEST_DIRECTORY}`);
     runCommand(`cd ${TEST_DIRECTORY}`);
     runCommand(`generate-project`);
@@ -53,6 +57,21 @@ describe("e2e", () => {
 
     console.log(templateFilePaths);
 
-    expect(templateFilePaths).toContain(`${TEST_DIRECTORY}/.package.json`);
+    expect(templateFilePaths).toContain(`${TEST_DIRECTORY}/package.json`);
+    expect(templateFilePaths).toContain(`${TEST_DIRECTORY}/yarn.lock`);
+    expect(templateFilePaths).toContain(`${TEST_DIRECTORY}/src/index.ts`);
+    expect(templateFilePaths).toContain(`${TEST_DIRECTORY}/dist/index.js`);
+  });
+
+  it.skip("files contain expected content", () => {
+    runCommand(`mkdir ${TEST_DIRECTORY}`);
+    runCommand(`cd ${TEST_DIRECTORY}`);
+    runCommand(`generate-project`);
+
+    const packageJson = readFileSync(`${TEST_DIRECTORY}/package.json`, "utf8");
+
+    expect(packageJson).toContain(`"name": "${MOCK_ANSWERS.packageName}"`);
+    expect(packageJson).toContain(`"author": "${MOCK_ANSWERS.author}"`);
+    expect(packageJson).toContain(`"description": "${MOCK_ANSWERS.packageDescription}"`);
   });
 });
