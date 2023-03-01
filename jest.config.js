b9a26d5b7f005eef76d9c69226e42bc616b90af6
lachiejames <lachie_james@hotmail.com.au>
2023-03-02 01:58:04 +1100

    test uplift

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
 module.exports = {
   preset: "ts-jest",
   testPathIgnorePatterns: ["<rootDir>/(coverage|dist).*/"],
+
+  // Tested via E2E, but coverage is not produced
+  coveragePathIgnorePatterns: ["<rootDir>/src/scaffold/runPostScaffoldSteps.ts", "<rootDir>/src/cli/setupCli.ts"],
+
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
+    },
+  },
 };
---
+++ src/io/insertGitIgnore.test.ts
@@ -0,0 +1,20 @@
+import fs from "fs-extra";
+
+import { cleanTestDir, testDir } from "../../testUtils";
+import {
+  insertGitIgnore,
+} from "..";
+
+describe("insertGitIgnore", () => {
+  afterEach(() => {
+    cleanTestDir();
+  });
+
+  it("creates files for the provided template", async () => {
+    expect(() => fs.readdirSync(testDir)).toThrowError(`ENOENT: no such file or directory, scandir '${testDir}'`);
+
+    insertGitIgnore(testDir);
+
+    expect(fs.readdirSync(testDir)).toContain(".gitignore");
+  });
+});
