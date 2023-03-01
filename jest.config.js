664c2c17ad7296a2c104cd1b47630ac77b02349c
lachiejames <lachie_james@hotmail.com.au>
2023-03-02 02:51:09 +1100

    update coverage

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   preset: "ts-jest",
   testPathIgnorePatterns: ["<rootDir>/(coverage|dist).*/"],
-
-  // Tested via E2E, but coverage is not produced
-  coveragePathIgnorePatterns: ["<rootDir>/src/scaffold/runPostScaffoldSteps.ts", "<rootDir>/src/cli/setupCli.ts"],
-
+  coveragePathIgnorePatterns: [
+    // Tested via E2E, but coverage is not produced
+    "<rootDir>/src/scaffold/runPostScaffoldSteps.ts",
+    "<rootDir>/src/cli/setupCli.ts",
+  ],
   coverageThreshold: {
     global: {
-      branches: 90,
-      functions: 90,
-      lines: 90,
-      statements: 90,
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
   },
 };
