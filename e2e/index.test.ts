12fb1c20ac9e0409c6b43636f03f996c0ea75095
lachiejames <lachie_james@hotmail.com.au>
2023-03-02 03:03:52 +1100

    add ts-node-docker test

--- e2e/index.test.ts
+++ e2e/index.test.ts
@@ -1,10 +1,7 @@
 /**
  * IMPORTANT: This test suite should only be run after executing `bash ./e2e/preTest.sh`, which installs the local
- * project as a global npm package and executes `generate-project` in a test directory.  This is the most comprehensive
- * way to verify that everything works before publishing to NPM.
- *
- * Ideally I would use TS to write those parts and just `execSync(<insert bash commands here>)`,
- * but that doesn't seem to work with the interactive CLI.  It just skips over the prompts.  So I'm using bash instead.
+ * project as a global npm package.  The tests then executes `generate-project` in a test directory (tempTestDir)
+ * This is the most comprehensive way to verify that everything works before publishing to NPM.
  */
 
 import childProcess from "child_process";
@@ -47,6 +44,44 @@ describe("ts-library", () => {
     expect(outputFilePaths).toContain(`${testDir}/package.json`);
     expect(outputFilePaths).toContain(`${testDir}/src/index.ts`);
 
+    // Ensure `yarn install` was successful
+    expect(outputFilePaths).toContain(`${testDir}/node_modules/typescript/package.json`);
+
+    // Ensure `yarn build` was successful
+    expect(outputFilePaths).toContain(`${testDir}/dist/index.js`);
+
+    // Ensure .gitignore is copied over (as a post-scaffold step)
+    expect(outputFilePaths).toContain(`${testDir}/.gitignore`);
+  });
+
+  it("files contain expected content", () => {
+    const packageJsonContents = fs.readFileSync(`${testDir}/package.json`, "utf8");
+
+    expect(packageJsonContents).toContain(`"name": "${defaultConfig.name}"`);
+    expect(packageJsonContents).toContain(`"description": "${defaultConfig.description}"`);
+    expect(packageJsonContents).toContain(`"author": "${defaultConfig.author}"`);
+  });
+});
+
+describe("ts-node-docker", () => {
+  beforeAll(() => {
+    executeCLI({ ...testConfig, template: "ts-node-docker" });
+  });
+
+  afterAll(() => {
+    // Clean up after ourselves
+    childProcess.execSync(`rm -rf ${testDir}`);
+    childProcess.execSync(`rm -rf *.tgz`);
+  });
+
+  it("produces the expected files", () => {
+    const outputFilePaths: string[] = glob.sync(`${testDir}/**`, { dot: true, nodir: true });
+
+    // Ensure src files were copied over
+    expect(outputFilePaths).toContain(`${testDir}/package.json`);
+    expect(outputFilePaths).toContain(`${testDir}/src/index.ts`);
+    expect(outputFilePaths).toContain(`${testDir}/Dockerfile`);
+    
     // Ensure `yarn install` was successful
     expect(outputFilePaths).toContain(`${testDir}/node_modules/typescript/package.json`);
 
--- e2e/ts-library/exec.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-set timeout 360
-
-yes "" | generate-project
