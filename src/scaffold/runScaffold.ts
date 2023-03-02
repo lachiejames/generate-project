9c09c6f81c32117f9dcad564076e8427f1291b03
lachiejames <lachie_james@hotmail.com.au>
2023-03-03 00:17:49 +1100

    remove irrelevant changes

--- src/scaffold/runScaffold.ts
+++ src/scaffold/runScaffold.ts
@@ -1,32 +1,15 @@
-import fs from "fs-extra";
-import glob from "glob";
-import nunjucks from "nunjucks";
-import path from "path";
-
-import { templates } from "../constants";
-import { rootDir } from "../io";
+import { getOutputFilePath, getTemplateFilePaths, writeTemplateToFile } from "../io";
 import { Config } from "../models";
+import loadNunjucksEnvironment from "./loadNunjucksEnvironment";
 
 async function runScaffold(config: Config): Promise<void> {
-  const fileLoader = new nunjucks.FileSystemLoader(rootDir);
-  const templateEnvironment = new nunjucks.Environment(fileLoader);
-
-  const templateFilePathsGlob = path.join(rootDir, "templates", config.template, "**");
-  const templateFilePaths = glob.sync(templateFilePathsGlob, { dot: true, nodir: true });
-  const templateNames = templates.map((template) => template.value);
-  if (templateFilePaths.length === 0) {
-    throw Error(`template not found: ${config.template}.  Must be one of: ${templateNames}`);
-  }
+  const templateEnvironment = loadNunjucksEnvironment();
+  const templateFilePaths = getTemplateFilePaths(config.template);
 
   for (const templateFilePath of templateFilePaths) {
     const template = templateEnvironment.getTemplate(templateFilePath);
-
-    const templateFilePathFromRoot = path.join(rootDir, "templates", config.template);
-    const relativeTemplateFilePath = path.relative(templateFilePathFromRoot, templateFilePath);
-    const outputFilePath = path.join(config.projectDir, relativeTemplateFilePath);
-
-    const outputString = template.render(config);
-    fs.outputFileSync(outputFilePath, outputString);
+    const outputFilePath = getOutputFilePath(config.template, templateFilePath, config.projectDir);
+    writeTemplateToFile(template, outputFilePath, config);
   }
 }
 
