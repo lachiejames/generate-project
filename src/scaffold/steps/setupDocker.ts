fa70180a77a4384db8fccd72480b3ab03d6f3528
lachiejames <lachie_james@hotmail.com.au>
2023-03-03 05:50:45 +1100

    update logs

--- src/scaffold/steps/setupDocker.ts
+++ src/scaffold/steps/setupDocker.ts
@@ -1,8 +1,8 @@
 import runStep from "./runStep";
 
 function setupDocker(projectName: string, projectDir: string) {
-  runStep(`docker build -t ${projectName} .`, "Building docker image with `docker build`", projectDir);
-  runStep(`docker run ${projectName}`, "Building docker image with `docker build`", projectDir);
+  runStep(`docker build -t ${projectName} .`, `Building docker image with \`docker build -t ${projectName} .\``, projectDir);
+  runStep(`docker run ${projectName}`, `Running docker image with \`docker run ${projectName}\``, projectDir);
 }
 
 export default setupDocker;
