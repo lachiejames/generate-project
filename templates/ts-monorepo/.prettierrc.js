714fe9c3a55b7f139051652342443289167a4a54
lachiejames <lachie_james@hotmail.com.au>
2023-03-03 06:19:00 +1100

    habaw

--- templates/ts-monorepo/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  printWidth: 120,
-  tabWidth: 2,
-  quoteProps: "consistent",
-  trailingComma: "all",
-};
--- templates/ts-monorepo/src/index.test.ts
+++
@@ -1,3 +0,0 @@
-it("my first test", () => {
-  expect(1 + 1).toEqual(2);
-});
