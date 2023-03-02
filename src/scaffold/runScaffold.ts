2da4b9bddf0272527e26e85d41febc32fe9dbb31
lachiejames <lachie_james@hotmail.com.au>
2023-03-03 00:02:56 +1100

    temporary, move everything into a single function to identify inefficiencies

--- src/scaffold/runScaffold.ts
+++ src/scaffold/runScaffold.ts
@@ -1,15 +1,32 @@
-import { getOutputFilePath, getTemplateFilePaths, writeTemplateToFile } from "../io";
+import fs from "fs-extra";
+import glob from "glob";
+import nunjucks from "nunjucks";
+import path from "path";
+
+import { templates } from "../constants";
+import { rootDir } from "../io";
 import { Config } from "../models";
-import loadNunjucksEnvironment from "./loadNunjucksEnvironment";
 
 async function runScaffold(config: Config): Promise<void> {
-  const templateEnvironment = loadNunjucksEnvironment();
-  const templateFilePaths = getTemplateFilePaths(config.template);
+  const fileLoader = new nunjucks.FileSystemLoader(rootDir);
+  const templateEnvironment = new nunjucks.Environment(fileLoader);
+
+  const templateFilePathsGlob = path.join(rootDir, "templates", config.template, "**");
+  const templateFilePaths = glob.sync(templateFilePathsGlob, { dot: true, nodir: true });
+  const templateNames = templates.map((template) => template.value);
+  if (templateFilePaths.length === 0) {
+    throw Error(`template not found: ${config.template}.  Must be one of: ${templateNames}`);
+  }
 
   for (const templateFilePath of templateFilePaths) {
     const template = templateEnvironment.getTemplate(templateFilePath);
-    const outputFilePath = getOutputFilePath(config.template, templateFilePath, config.projectDir);
-    writeTemplateToFile(template, outputFilePath, config);
+
+    const templateFilePathFromRoot = path.join(rootDir, "templates", config.template);
+    const relativeTemplateFilePath = path.relative(templateFilePathFromRoot, templateFilePath);
+    const outputFilePath = path.join(config.projectDir, relativeTemplateFilePath);
+
+    const outputString = template.render(config);
+    fs.outputFileSync(outputFilePath, outputString);
   }
 }
 
