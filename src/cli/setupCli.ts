aeefc520f802e1b4771332c0c756175b258331fc
lachiejames <lachie_james@hotmail.com.au>
2023-03-03 21:26:54 +1100

    update cli setup

--- src/cli/setupCli.ts
+++ src/cli/setupCli.ts
@@ -1,18 +1,24 @@
 import commander from "commander";
 
-import { GPConfig } from "../models";
+import { GPConfig, GPTemplateName } from "../models";
 
 function setupCli(): Partial<GPConfig> {
   const program = new commander.Command("generate-project");
+
   program
-    .option("-t, --templateName <string>", "Selected template")
-    .option("-n, --projectName <string>", "Project name")
-    .option("-d, --projectDescription <string>", "Project description")
-    .option("-a, --projectAuthor <string>", "Project author")
-    .option("-p, --projectDir <string>", "Project location, defaults to $(pwd)")
-    .parse(process.argv);
+    .addArgument(
+      new commander.Argument("template", "Selected template").choices(Object.values(GPTemplateName)).argOptional(),
+    )
+    .addOption(new commander.Option("-n, --projectName <string>", "Project name"))
+    .addOption(new commander.Option("-d, --projectDescription <string>", "Project description"))
+    .addOption(new commander.Option("-a, --projectAuthor <string>", "Project author"))
+    .addOption(new commander.Option("-p, --projectDir <string>", "Project location, defaults to $(pwd)"));
+
+  program.parse(process.argv);
 
   return program.opts();
 }
+
+console.log(setupCli());
 
 export default setupCli;
